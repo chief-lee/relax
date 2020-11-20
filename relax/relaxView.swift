71d19d1ea43f87b531c5067ae0d08e66bf94825d
李锦 <lijin4@huya.com>
2020-11-20 15:15:05 +0800

    休息提醒改为全屏覆盖显示

---
+++ relax/relaxView.swift
@@ -0,0 +1,20 @@
+//
+//  relaxView.swift
+//  relax
+//
+//  Created by 李锦 on 2020/11/20.
+//
+
+import SwiftUI
+
+struct relaxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct relaxView_Previews: PreviewProvider {
+    static var previews: some View {
+        relaxView()
+    }
+}
