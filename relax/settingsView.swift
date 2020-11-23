15bea1064e99154b00ec1f673eb8a4bbb5c2047f
李锦 <lijin4@huya.com>
2020-11-23 10:19:40 +0800

    增加修改时间选项

---
+++ relax/settingsView.swift
@@ -0,0 +1,20 @@
+//
+//  settingsView.swift
+//  relax
+//
+//  Created by 李锦 on 2020/11/20.
+//
+
+import SwiftUI
+
+struct settingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct settingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        settingsView()
+    }
+}
